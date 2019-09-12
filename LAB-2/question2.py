57821bef7120d26ff6bd5eb0af7b7bbb6470449f
ABHISHEK TIBREWAL <31183355+tibra27@users.noreply.github.com>
2019-09-12 12:32:30 +0530

    SLCPTA

---
+++ LAB-2/question2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#ASSIGNMENT-2-QUESTION-2
+#2016UCP1103
+#ABHISHEK TIBREWAL
+#SINGLE LAYER CONTINUOUS PERCEPTRON TRAINING ALGORITHM
+######################################################
+
+
+import math
+import numpy as np
+
+def Activation_sigmoid(net):
+    x=(2/(1+(math.e**(-1*net)))-1)
+    return x
+    
+
+w=[[0],[0],[0],[0]]
+weight=np.array(w)
+out=[-1,1,-1,1,-1,1,-1,1]
+I1=np.array([[0],[0],[0],[1]])
+I2=np.array([[0],[0],[1],[1]])
+I3=np.array([[0],[1],[0],[1]])
+I4=np.array([[0],[1],[1],[1]])
+I5=np.array([[1],[0],[0],[1]])
+I6=np.array([[1],[0],[1],[1]])
+I7=np.array([[1],[1],[0],[1]])
+I8=np.array([[1],[1],[1],[1]])
+I=[I1,I2,I3,I4,I5,I6,I7,I8]
+c=0.9
+epoc=1
+while(True):
+    print("Epoc-",epoc)
+    f=0
+    for i in range(len(I)):
+        net=np.dot(np.transpose(w),I[i])
+        print(net)
+        response=Activation_sigmoid(net)
+        w=w+((c*(out[i]-response)*(1-response*response)*I[i])/2)
+        if(out[i]-response!=0):
+            f+=1
+        for j in w:
+            print(j[0],end=" ")
+        print("\n")
+    epoc+=1
+    if(f==0):
+        break
+
+
+# In[ ]:
+
+
+
+
